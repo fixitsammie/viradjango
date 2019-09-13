319088576cb5208ee28154a07d67e412ceaeea6d
Samuel Adewole <samadewole381@gmail.com>
2019-09-13 15:30:08 +0100

    more changes to heroku push

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -18,7 +18,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
 
 # Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
 ALLOWED_HOSTS = ['*']
