de084485940c840eff3a1477cc5d40c2828f44a8
Samuel Adewole <samadewole381@gmail.com>
2019-09-15 13:21:08 +0100

    changed url for request promise

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -149,7 +149,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
